380eeefdaf4689cc2ef9562048f6a76457cf8372
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-08-01 19:16:26 -0700

    Prepare for next automation part when image file will be merged

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const git = require('isomorphic-git');
 
 const egVersion = process.env.CIRCLE_TAG.substr(1); // GitHub Tags is vx.y.z — we need just x.y.z
+const token = process.env.GITHUB_TOKEN;
 
 ejs.renderFile('./Dockerfile_template', { egVersion }, {}, (err, str) => {
   if (err)
@@ -15,7 +16,8 @@ ejs.renderFile('./Dockerfile_template', { egVersion }, {}, (err, str) => {
   git.branch({ ...repo, ref: 'feat/update' })
     .then(() => git.checkout({ ...repo, ref: 'feat/update' }))
     .then(() => git.add({ ...repo, filepath: './alpine/Dockerfile' }))
-    .then(() => git.commit({ ...repo, message: 'Nasino pariosino', author: { name: 'EGBot', email: 'info@express-gateway.io' } }))
-    .then(() => git.push({ ...repo, remote: 'origin', ref: 'feat/update', }))
-    .catch(err => console.error)
+    .then(() => git.commit({ ...repo, message: 'Update', author: { name: 'Vincenzo Chianese', email: 'vincenz.chianese@icloud.com' } }))
+    .then(sha => Promise.all([sha, git.push({ ...repo, remote: 'origin', ref: 'feat/update', token, force: true })]))
+    .then(console.log)
+    .catch(console.error)
 })
