a9f6857d5cac1b88b00bf569fe8481fa7c9abfdc
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-08-03 11:41:59 +0200

    Do not hardcode name and email

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const git = require('isomorphic-git');
 
 const egVersion = process.env.CIRCLE_TAG.substr(1); // GitHub Tags is vx.y.z — we need just x.y.z
 const token = process.env.GITHUB_TOKEN;
+const name = process.env.GITHUB_NAME;
+const email = process.env.GITHUB_EMAIL;
+const repo = { fs, dir: '.' }
 
-ejs.renderFile('./Dockerfile_template', { egVersion }, {}, (err, str) => {
-  if (err)
-    return console.error(err);
+git.branch({ ...repo, ref: `feat/update_${egVersion}` })
+  .then(() => git.checkout({ ...repo, ref: `feat/update_${egVersion}` }))
+  .then(() => new Promise((resolve, reject) => {
+    ejs.renderFile('./Dockerfile_template', { egVersion }, {}, (err, str) => {
+      if (err)
+        return reject(err);
 
-  fs.writeFileSync('./alpine/Dockerfile', str);
+      fs.writeFileSync('./alpine/Dockerfile', str);
 
-  const repo = { fs, dir: '.' }
-
-  git.branch({ ...repo, ref: 'feat/update' })
-    .then(() => git.checkout({ ...repo, ref: 'feat/update' }))
-    .then(() => git.add({ ...repo, filepath: './alpine/Dockerfile' }))
-    .then(() => git.commit({ ...repo, message: 'Update', author: { name: 'Vincenzo Chianese', email: 'vincenz.chianese@icloud.com' } }))
-    .then(sha => Promise.all([sha, git.push({ ...repo, remote: 'origin', ref: 'feat/update', token, force: true })]))
-    .then(console.log)
-    .catch(console.error)
-})
+      return git.add({ ...repo, filepath: './alpine/Dockerfile' }).then(resolve).catch(reject);
+    })
+  }))
+  .then(() => {
+    return git.commit({ ...repo, message: 'Update', author: { name, email } })
+  })
+  .then(sha => Promise.all([sha, git.push({ ...repo, remote: 'origin', ref: `feat/update_${egVersion}`, token, force: true })]))
+  .then(console.log)
+  .catch(console.error)
