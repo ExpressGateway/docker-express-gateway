76ef66743b8e9c81d1bcc01ee2d63ae917a4ac1b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-08-24 09:04:12 +0200

    Add helm chart creation

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 const ejs = require('ejs');
 const fs = require('fs');
-const git = require('isomorphic-git');
+const async = require('async');
 
 const egVersion = process.env.CIRCLE_TAG.substr(1); // GitHub Tags is vx.y.z — we need just x.y.z
-const token = process.env.GITHUB_TOKEN;
-const name = process.env.GITHUB_NAME;
-const email = process.env.GITHUB_EMAIL;
-const repo = { fs, dir: '.' }
+const chartVersion = process.env.HELM_CHART_VERSION;
 
-git.branch({ ...repo, ref: `feat/update_${egVersion}` })
-  .then(() => git.checkout({ ...repo, ref: `feat/update_${egVersion}` }))
-  .then(() => new Promise((resolve, reject) => {
-    ejs.renderFile('./Dockerfile_template', { egVersion }, {}, (err, str) => {
-      if (err)
-        return reject(err);
+async.parallel([
+  (cb) => ejs.renderFile('./Dockerfile_template', { egVersion }, {}, cb),
+  (cb) => ejs.renderFile('./Helmfile_template', { egVersion, chartVersion }, {}, cb)
+], (err, [Dockerfile, Helmfile]) => {
+  if (err)
+    return console.error(err);
 
-      fs.writeFileSync('./alpine/Dockerfile', str);
+  fs.writeFileSync('./alpine/Dockerfile', Dockerfile);
+  fs.writeFileSync('./Helmfile', Helmfile);
+});
 
-      return git.add({ ...repo, filepath: 'alpine/Dockerfile' }).then(resolve).catch(reject);
-    })
-  }))
-  .then(() => git.commit({ ...repo, message: 'Update', author: { name, email } }))
-  .then(sha => Promise.all([sha, git.push({ ...repo, remote: 'origin', ref: `feat/update_${egVersion}`, token, force: true })]))
-  .then(console.log)
-  .catch(console.error)
