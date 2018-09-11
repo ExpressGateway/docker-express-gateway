9b918cbfea0960cd53643f29b94fad890cae940a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-09-11 10:52:29 +0200

    Chart version separate

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const async = require('async');
 
 const egVersion = process.env.CIRCLE_TAG.substr(1); // GitHub Tags is vx.y.z — we need just x.y.z
+const chartVersion = process.env.EG_CHART_VERSION;
 
 async.parallel([
   (cb) => ejs.renderFile('./Dockerfile_template', { egVersion }, {}, cb),
-  (cb) => ejs.renderFile('./Helmfile_template', { egVersion, chartVersion: egVersion }, {}, cb),
+  (cb) => ejs.renderFile('./Helmfile_template', { egVersion, chartVersion }, {}, cb),
   (cb) => ejs.renderFile('./Helmvaluefile_template', { egDockerVersion: process.env.CIRCLE_TAG }, {}, cb)
 ], (err, [Dockerfile, Helmfile, Helmvaluefile]) => {
   if (err)
