4785e2cbd500ff71d881848700b8ab80868cbfb4
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-09-20 15:12:44 +0200

    Create helm readme

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const egVersion = process.env.CIRCLE_TAG.substr(1); // GitHub Tags is vx.y.z —
 const chartVersion = process.env.EG_CHART_VERSION;
 
 async.parallel([
-  (cb) => ejs.renderFile('./Dockerfile_template', { egVersion }, {}, cb),
-  (cb) => ejs.renderFile('./Helmfile_template', { egVersion, chartVersion }, {}, cb),
-  (cb) => ejs.renderFile('./Helmvaluefile_template', { egDockerVersion: process.env.CIRCLE_TAG }, {}, cb)
-], (err, [Dockerfile, Helmfile, Helmvaluefile]) => {
+  cb => ejs.renderFile('./Dockerfile_template', { egVersion }, {}, cb),
+  cb => ejs.renderFile('./Helmfile_template', { egVersion, chartVersion }, {}, cb),
+  cb => ejs.renderFile('./Helmvaluefile_template', { egDockerVersion: process.env.CIRCLE_TAG }, {}, cb),
+  cb => ejs.renderFile('./Helmreadme_template', { egVersion }, {}, cb)
+], (err, [Dockerfile, Helmfile, Helmvaluefile, Helmreadme]) => {
   if (err)
     return console.error(err);
 
-  fs.writeFileSync('./alpine/Dockerfile', Dockerfile);
-  fs.writeFileSync('./Helmfile', Helmfile);
-  fs.writeFileSync('./Helmvaluefile', Helmvaluefile);
+  async.parallel([
+    cb => fs.writeFile('./alpine/Dockerfile', Dockerfile, cb),
+    cb => fs.writeFile('./Helmfile', Helmfile, cb),
+    cb => fs.writeFile('./Helmvaluefile', Helmvaluefile, cb),
+    cb => fs.writeFile('./Helmreadme', Helmreadme, cb),
+  ], err => {
+    if (err)
+      return console.error(err);
+  });
 });
